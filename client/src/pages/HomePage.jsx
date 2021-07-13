00430639623c240e87f3d9aed5873b9f0ffcb991
Tomsylva <tomsylvanz@gmail.com>
2021-07-13 11:07:16 +0200

    successfully receiving midi messages from synth

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,30 +1,57 @@
 import "../App.css";
-import * as JZZ from "jzz";
+// import * as JZZ from "jzz";
 
 function HomePage() {
-  const onSuccess = function () {
-    const midiPort = JZZ()
-      .openMidiIn("Playtron")
-      .then(console.log("Playtron connected"), (midiPort) => {
-        midiPort
-          .noteOn(0, "C2", 127)
-          .then(console.log("NOTEON"))
-          .wait(500)
-          .noteOff(0, "C2")
-          .close()
-          .then(console.log("NOTEOFF"));
-      });
-    console.log("PORT INFO: ", midiPort.info());
-    // const closeMidi = function () {
-    //   midiPort.close().then(console.log("MIDI PORT CLOSED"));
-    // };
+  const onSuccess = function (midiAccess) {
+    console.log("MIDI ACCESS: ", midiAccess);
+    const inputs = midiAccess.inputs;
+    console.log("INPUTS: ", inputs);
+    const outputs = midiAccess.outputs;
+    console.log("OUTPUTS: ", outputs);
+    for (let input of inputs.values()) {
+      input.onmidimessage = getMIDIMessage;
+      console.log("HERE");
+    }
+    function getMIDIMessage(midiMessage) {
+      console.log(midiMessage);
+      const command = midiMessage.data[0];
+      const note = midiMessage.data[1];
+      const velocity = midiMessage.data.length > 2 ? midiMessage.data[2] : 0;
+
+      // eslint-disable-next-line
+      switch (command) {
+        case 144:
+          if (velocity > 0) {
+            // JZZ.noteOn(note, velocity);
+            console.log("NOTE ON", note);
+          } else {
+            // JZZ.noteOff(note);
+            console.log("NOTE OFF");
+          }
+          break;
+        case 128:
+          // JZZ.noteOff(note);
+          console.log("NOTE OFF");
+          break;
+      }
+    }
+
+    // const midiPort = JZZ()
+    //   .openMidiIn("Playtron")
+    //   .then((midiPort) => {
+    //     console.log("Playtron connected");
+    //     const inputs = midiPort.inputs;
+    //     console.log("inputs", inputs);
+    //   });
+    // console.log("PORT INFO: ", midiPort.info());
   };
 
   const onFail = function () {
     console.log("MIDI FAIL");
   };
 
-  JZZ.requestMIDIAccess().then(onSuccess, onFail);
+  // JZZ.requestMIDIAccess().then(onSuccess, onFail);
+  navigator.requestMIDIAccess().then(onSuccess, onFail);
 
   return (
     <div className="App">
