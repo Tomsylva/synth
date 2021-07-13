1e93ca1a1e59027fe670dbfa50ce930402027fbc
Tomsylva <tomsylvanz@gmail.com>
2021-07-13 15:45:03 +0200

    added framer motion, added error messages, added instruments names on screen, was amazing

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,69 +1,95 @@
-import "../App.css";
+import React from "react";
+import { motion } from "framer-motion";
 // import * as JZZ from "jzz";
 
 function HomePage() {
+  const [currentInstrument, setCurrentInstrument] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
+
   const onSuccess = function (midiAccess) {
-    console.log("MIDI ACCESS: ", midiAccess);
     const inputs = midiAccess.inputs;
-    console.log("INPUTS: ", inputs);
-    const outputs = midiAccess.outputs;
-    console.log("OUTPUTS: ", outputs);
+    // const outputs = midiAccess.outputs;
     for (let input of inputs.values()) {
       input.onmidimessage = getMIDIMessage;
     }
     function getMIDIMessage(midiMessage) {
-      console.log(midiMessage);
       const command = midiMessage.data[0];
       const note = midiMessage.data[1];
       const velocity = midiMessage.data.length > 2 ? midiMessage.data[2] : 0;
+      const instruments = {
+        36: "BASS",
+        37: "SNARE",
+        38: "HATS",
+        39: "CRASH",
+        40: "set me",
+        41: "set me",
+        42: "set me",
+        43: "set me",
+        44: "set me",
+        45: "set me",
+        46: "set me",
+        47: "set me",
+        48: "set me",
+        49: "set me",
+        50: "set me",
+        51: "set me",
+      };
 
       // eslint-disable-next-line
       switch (command) {
         case 144:
           if (velocity > 0) {
             // JZZ.noteOn(note, velocity);
-            console.log("NOTE ON", note);
+            const instrument = instruments[note];
+            setCurrentInstrument(instrument);
+            if (midiMessage.target.name !== "Playtron") {
+              setErrorMessage(
+                "This online synth has been designed to work with a Playtron by Playtronica, however, all midi devices will work between C1 and D#2."
+              );
+            }
           } else {
             // JZZ.noteOff(note);
-            console.log("NOTE OFF");
+            endOfNote();
           }
           break;
         case 128:
           // JZZ.noteOff(note);
-          console.log("NOTE OFF");
+          endOfNote();
+          function endOfNote() {
+            setTimeout(function () {
+              setCurrentInstrument("");
+            }, 300);
+          }
           break;
       }
     }
-
-    // NOTE IS NUMBER
-    // IF CASE FOR EACH NUMBER OR
-    // SET UP AN OBJECT OF NOTE NUMBERS AND ACCOMPANYING SOUNDS AND REFERENCE
-    // CHECK OUT https://github.com/jonobr1/Neuronal-Synchrony/blob/master/src/main.js LINE 329
-    // 36 37 38 39
-    // 40 41 42 43
-    // 44 45 46 47
-    // 48 49 50 51
-
-    // const midiPort = JZZ()
-    //   .openMidiIn("Playtron")
-    //   .then((midiPort) => {
-    //     console.log("Playtron connected");
-    //     const inputs = midiPort.inputs;
-    //     console.log("inputs", inputs);
-    //   });
-    // console.log("PORT INFO: ", midiPort.info());
   };
 
   const onFail = function () {
-    console.log("MIDI FAIL");
+    setErrorMessage("Could not connect to your midi device");
   };
 
-  // JZZ.requestMIDIAccess().then(onSuccess, onFail);
   navigator.requestMIDIAccess().then(onSuccess, onFail);
 
   return (
     <div className="App">
       <h1 id="maintitle">PLAYTRON SYNTH</h1>
+      {currentInstrument ? (
+        <motion.div
+          initial="visible"
+          animate="hidden"
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+            transition: {
+              delay: 0.01,
+            },
+          }}
+        >
+          <h2>{currentInstrument}</h2>
+        </motion.div>
+      ) : null}
+      {errorMessage ? <h3>{errorMessage}</h3> : null}
     </div>
   );
 }
