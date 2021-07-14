56e96297f41110159d1d28463229eeb872b27123
Tomsylva <tomsylvanz@gmail.com>
2021-07-14 15:57:48 +0200

    this is what I did while the internet was doooown

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { motion } from "framer-motion";
-// import * as JZZ from "jzz";
 
 function HomePage() {
   const [currentInstrument, setCurrentInstrument] = React.useState("");
   const [errorMessage, setErrorMessage] = React.useState("");
+  const [page, setPage] = React.useState("home");
+  const [touchMe, setTouchMe] = React.useState(true);
 
   const onSuccess = function (midiAccess) {
     const inputs = midiAccess.inputs;
-    // const outputs = midiAccess.outputs;
     for (let input of inputs.values()) {
       input.onmidimessage = getMIDIMessage;
     }
@@ -17,43 +17,41 @@ function HomePage() {
       const note = midiMessage.data[1];
       const velocity = midiMessage.data.length > 2 ? midiMessage.data[2] : 0;
       const instruments = {
-        36: "BASS",
-        37: "SNARE",
-        38: "HATS",
-        39: "CRASH",
-        40: "set me",
-        41: "set me",
-        42: "set me",
-        43: "set me",
-        44: "set me",
-        45: "set me",
-        46: "set me",
-        47: "set me",
-        48: "set me",
-        49: "set me",
-        50: "set me",
-        51: "set me",
+        36: { instrumentName: "KICK", image: "#", sound: "#" },
+        37: { instrumentName: "SNARE", image: "#", sound: "#" },
+        38: { instrumentName: "HATS", image: "#", sound: "#" },
+        39: { instrumentName: "CRASH", image: "#", sound: "#" },
+        40: { instrumentName: "BASS E", image: "#", sound: "#" },
+        41: { instrumentName: "BASS F#", image: "#", sound: "#" },
+        42: { instrumentName: "BASS G#", image: "#", sound: "#" },
+        43: { instrumentName: "BASS A", image: "#", sound: "#" },
+        44: { instrumentName: "MELODY E", image: "#", sound: "#" },
+        45: { instrumentName: "MELODY A", image: "#", sound: "#" },
+        46: { instrumentName: "MELODY G#", image: "#", sound: "#" },
+        47: { instrumentName: "MEDLODY C#", image: "#", sound: "#" },
+        48: { instrumentName: "BEEPS E", image: "#", sound: "#" },
+        49: { instrumentName: "BEEPS C#", image: "#", sound: "#" },
+        50: { instrumentName: "BANANA!", image: "#", sound: "#" },
+        51: { instrumentName: "SPOON!", image: "#", sound: "#" },
       };
 
       // eslint-disable-next-line
       switch (command) {
         case 144:
           if (velocity > 0) {
-            // JZZ.noteOn(note, velocity);
-            const instrument = instruments[note];
+            const instrument = instruments[note].instrumentName;
             setCurrentInstrument(instrument);
+            setTouchMe(false);
             if (midiMessage.target.name !== "Playtron") {
               setErrorMessage(
                 "This online synth has been designed to work with a Playtron by Playtronica, however, all midi devices will work between C1 and D#2."
               );
             }
           } else {
-            // JZZ.noteOff(note);
             endOfNote();
           }
           break;
         case 128:
-          // JZZ.noteOff(note);
           endOfNote();
           function endOfNote() {
             setTimeout(function () {
@@ -69,27 +67,40 @@ function HomePage() {
     setErrorMessage("Could not connect to your midi device");
   };
 
+  const loadSynth = function () {
+    setPage("synth");
+  };
+
   navigator.requestMIDIAccess().then(onSuccess, onFail);
 
   return (
     <div className="App">
-      <h1 id="maintitle">PLAYTRON SYNTH</h1>
-      {currentInstrument ? (
-        <motion.div
-          initial="visible"
-          animate="hidden"
-          variants={{
-            hidden: { opacity: 0 },
-            visible: { opacity: 1 },
-            transition: {
-              delay: 0.01,
-            },
-          }}
-        >
-          <h2>{currentInstrument}</h2>
-        </motion.div>
-      ) : null}
-      {errorMessage ? <h3>{errorMessage}</h3> : null}
+      {page === "home" ? (
+        <div>
+          <h1>PLAYTRON SYNTH!!</h1>
+          <button onClick={loadSynth}>Let's get started!</button>
+        </div>
+      ) : (
+        <div className="snyth-page">
+          {touchMe ? <h3>Go on, touch me...</h3> : null}
+          {currentInstrument ? (
+            <motion.div
+              initial="visible"
+              animate="hidden"
+              variants={{
+                hidden: { opacity: 0 },
+                visible: { opacity: 1 },
+                transition: {
+                  delay: 0.01,
+                },
+              }}
+            >
+              <h2>{currentInstrument}</h2>
+            </motion.div>
+          ) : null}
+          {errorMessage ? <h3>{errorMessage}</h3> : null}
+        </div>
+      )}
     </div>
   );
 }
